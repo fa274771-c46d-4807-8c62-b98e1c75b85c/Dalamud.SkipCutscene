9001e74e41c9fc7c937084c07121d361538b0bc7
= <=>
2025-04-11 15:32:59 +0200

    Baby's first config

---
+++ src/Dalamud.Plugin.SkipCutscene/Config.cs
@@ -0,0 +1,12 @@
+﻿using Dalamud.Configuration;
+
+namespace Dalamud.Plugin.SkipCutscene;
+
+public class Config : IPluginConfiguration
+{
+    public int Version { get; set; } = 1;
+
+    public bool IsEnabled { get; set; } = true;
+
+    public string Command { get ; set; } = "sc";
+}
---
+++ src/Dalamud.Plugin.SkipCutscene/Views/ConfigWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace Dalamud.Plugin.SkipCutscene.Views;
+
+internal class ConfigWindow(
+    Config config,
+    IReadOnlyDictionary<string, CommandInfo> commands
+    ) : Window($"{nameof(SkipCutscene)} configuration"), IDisposable
+{
+    public override void Draw()
+    {
+        var command = config.Command;
+        var oldCommand = command;
+        //SkipCutscene.PluginLog.Logger.Information("{Command}, {OldCommand}", command, oldCommand);
+
+        ImGui.InputText("Command to use?", ref command, 250, ImGuiInputTextFlags.CharsNoBlank);
+        if (command != oldCommand)
+        {
+            SkipCutscene.CommandManager.RemoveHandler($"/{oldCommand}");
+            SkipCutscene.CommandManager.AddHandler($"/{command}", commands[nameof(SkipCutscene.SanityCheck)]);
+
+            config.Command = command;
+            SkipCutscene.Interface.SavePluginConfig(config);
+        }
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
