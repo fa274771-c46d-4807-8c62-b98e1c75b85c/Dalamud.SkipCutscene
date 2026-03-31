830c34d2295d1cacbda1225aea317d5b160cc48f
= <=>
2026-03-31 16:10:51 +0200

    Refactor, add dtr, add party check, revise config in general

---
+++ src/Dalamud.Plugin.SkipCutscene/Extensions/SeStringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Dalamud.Game.Text.SeStringHandling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dalamud.Plugin.SkipCutscene.Extensions;
+
+public static class SeStringExtensions
+{
+    public static SeStringBuilder Add(this SeStringBuilder sb, SeString seString)
+    {
+        foreach(var payload in seString.Payloads)
+        {
+            sb.Add(payload);
+        }
+        return sb;
+    }
+}
---
+++ src/Dalamud.Plugin.SkipCutscene/SkipCutscene.services.cs
@@ -0,0 +1,47 @@
+﻿using Dalamud.IoC;
+using Dalamud.Plugin.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Dalamud.Plugin.SkipCutscene;
+
+public partial class SkipCutscene: IDalamudPlugin
+{
+    [PluginService]
+    [NotNull]
+    public static IDalamudPluginInterface? Interface { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IGameInteropProvider? InteropProvider { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static ICommandManager? CommandManager { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IFramework? Framework { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IChatGui? ChatGui { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IPluginLog? PluginLog { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IClientState? ClientState { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IPartyList? PartyList { get; private set; }
+
+    [PluginService]
+    [NotNull]
+    public static IDtrBar? DtrBar { get; private set; }
+}
